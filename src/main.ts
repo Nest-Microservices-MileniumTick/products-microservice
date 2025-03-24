bd0f36abdbf8354331b295aa542e6251749b86be
Josue Chavarria M <jchavarria@jchavarria.>
2025-03-24 02:35:42 -0600

    fix: add lsiten to ms

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import {MicroserviceOptions, Transport} from '@nestjs/microservices'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { ENV } from './config/env';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
       port: ENV.PORT
     }
   });
+
   const logger = new Logger('Main')
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true
   }))
+
+  await app.listen()
 
   logger.log(`Products microservice running on port ${ENV.PORT}`)
 }
