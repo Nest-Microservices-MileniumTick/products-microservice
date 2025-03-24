b404376c298f266c5b094757689f46ccdcfb0381
Josue Chavarria M <jchavarria@jchavarria.>
2025-03-24 02:36:56 -0600

    feat: format and add rpc exception

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,16 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('ProductsService')
 
-  async onModuleInit() {
-    await this.$connect()
+  onModuleInit() {
+    this.$connect()
     this.logger.log('Database Connected')
   }
 
@@ -22,14 +23,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto
 
-    const records = await this.product.count({where:{deletedAt: null}})
+    const records = await this.product.count({ where: { deletedAt: null } })
     const lastPage = Math.ceil(records / limit)
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
-        where: {deletedAt: null}
+        where: { deletedAt: null }
       }),
       meta: {
         page,
@@ -44,7 +45,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: { id, deletedAt: null }
     });
 
-    if(!product) throw new NotFoundException(`Product with id #${id} not exist`);
+    if (!product) throw new RpcException({
+      message: `Product with id #${id} not found`,
+      status: HttpStatus.BAD_REQUEST
+    });
 
     return product;
   }
@@ -52,17 +56,17 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async update(id: number, updateProductDto: UpdateProductDto) {
     await this.findOne(id)
 
-    const {id:_, ...data} = updateProductDto
+    const { id: _, ...data } = updateProductDto
 
     return this.product.update({
       data,
-      where: {id}
+      where: { id }
     });
   }
 
   async remove(id: number) {
     await this.findOne(id)
 
-    return this.product.update({where: {id}, data: {deletedAt: new Date()}});
+    return this.product.update({ where: { id }, data: { deletedAt: new Date() } });
   }
 }
